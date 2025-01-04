9d71f7c3fd234ddd79d14f2b68d2d6e1b033cb9c
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2025-01-03 16:42:05 -0800

    Fix wrong server action implementation (#111)

--- src/app/add/repositories/addrepository.tsx
+++ src/app/add/repositories/addrepository.tsx
@@ -1,9 +1,6 @@
 import InsertQueue from "@/service/insert-queue";
 
-export async function addRepository(formData: FormData) {
-  const owner: string = formData.get('owner') as string;
-  const repo: string = formData.get('repo') as string;
-
+export async function addRepository(owner: string, repo: string) {
   if (!owner || !repo) {
     return { success: false, error: 'Both owner and repository names are required.' };
   }
