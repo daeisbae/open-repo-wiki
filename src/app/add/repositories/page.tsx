457e4e4fa609b7af30348754b7b43db7f285d2be
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2025-01-03 19:20:10 -0800

    Fix wrong server action implementation (#111)

--- src/app/add/repositories/page.tsx
+++ src/app/add/repositories/page.tsx
@@ -6,19 +6,19 @@ import { addRepository } from '@/app/add/repositories/addrepository';
 export default function Page() {
   const [response, setResponse] = useState(undefined);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const owner: string = formData.get('owner') as string;
-    const repo: string = formData.get('repo') as string;
-    const result = await addRepository(owner, repo);
-    setResponse(result);
-  };
-
   return (
     <div className="flex flex-col items-center justify-center w-full h-full overflow-y-hidden">
       <h1 className="font-bold text-lg sm:text-2xl md:text-3xl pb-4">Add Repository</h1>
-      <Form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full max-w-md p-6 border rounded-lg shadow-sm">
+      <Form action={async (formData) => {
+        const owner = formData.get('owner') as string;
+        const repo = formData.get('repo') as string;
+        return await addRepository(owner, repo);
+      }} onSubmit={async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const result = await addRepository(formData.get('owner') as string, formData.get('repo') as string);
+        setResponse(result);
+      }} className="flex flex-col gap-4 w-full max-w-md p-6 border rounded-lg shadow-sm">
         <div className="flex flex-col gap-4">
           <input
             className="p-4 border rounded-md text-center"
