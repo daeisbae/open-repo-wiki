f7a3d76380868f6271c98a72b4e7e5a5f4e842d9
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2025-01-04 02:41:47 -0800

    Implement collapsible heading for summarization page (#125)

--- src/app/[ownerSlug]/[repoSlug]/formatter.ts
+++ src/app/[ownerSlug]/[repoSlug]/formatter.ts
@@ -1,4 +1,4 @@
-
+import { FileData } from '@/db/models/file'
 import { FullFolder, FullRepository } from '@/service/get-db'
 
 export function formatToMarkdownSections(fullRepo: FullRepository): string[] {
@@ -8,10 +8,9 @@ export function formatToMarkdownSections(fullRepo: FullRepository): string[] {
 
     const url = `https://github.com/${repo.owner}/${repo.repo}/blob/${branches?.last_commit_sha}`
 
-    // We'll accumulate our markdown sections in an array
     const sections: string[] = []
     if (folders.length > 0) {
-        recursiveFolderToMarkdownSections(folders[0], url, '#', sections)
+        recursiveFolderToMarkdownSections(folders[0], url, 1, sections)
     }
 
     return sections
@@ -20,38 +19,39 @@ export function formatToMarkdownSections(fullRepo: FullRepository): string[] {
 function recursiveFolderToMarkdownSections(
     folder: FullFolder,
     url: string,
-    subfolderHeading: string,
+    subfolderHeadingNo: number,
     sections: string[]
 ) {
-    let markdown = `${subfolderHeading} ${folder.usage}\n`
+    let markdown = `<CollapsibleHeader heading="${folder.usage}" level={${subfolderHeadingNo}} open={true}>\n`
     markdown += `---\n`
     markdown += folder.path
         ? `- Reference: [\`${folder.path}\`](${url}/${folder.path}) \n`
         : ''
     markdown += `\n${folder.ai_summary}\n`
 
-    // folder.files.filter(isNotMarkdownFile).forEach((file) => {
-    //     markdown += `###### ${file.usage}\n`
-    //     markdown += `---\n`
-    //     markdown += file.name
-    //         ? `- Reference: [\`${file.name}\`](${url}/${file.name}) \n`
-    //         : ''
-    //     markdown += `\n${file.ai_summary}\n`
-    // })
+    folder.files.filter(isNotMarkdownFile).forEach((file) => {
+        markdown += `<CollapsibleHeader heading="${file.usage}" level={6}>\n`
+        markdown += `---\n`
+        markdown += file.name
+            ? `- Reference: [\`${file.name}\`](${url}/${file.name}) \n`
+            : ''
+        markdown += `\n${file.ai_summary}\n`
+        markdown += `</CollapsibleHeader>\n`
+    })
+    markdown += `</CollapsibleHeader>\n`
 
-    // Each "folder section" is one chunk of markdown
     sections.push(markdown)
 
     folder.subfolders.forEach((subfolder) => {
         recursiveFolderToMarkdownSections(
             subfolder,
             url,
-            subfolderHeading.length >= 5 ? subfolderHeading : subfolderHeading + '#',
+            subfolderHeadingNo >= 5 ? subfolderHeadingNo : ++subfolderHeadingNo,
             sections
         )
     })
 }
 
-// function isNotMarkdownFile(file: FileData): boolean {
-//     return !file.name.includes('.md')
-// }
+function isNotMarkdownFile(file: FileData): boolean {
+    return !file.name.includes('.md')
+}
