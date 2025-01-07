9877369c9cbd99c57cd01abf5996d596adf7e5dd
Dae❤️ <74119677+daeisbae@users.noreply.github.com>
2025-01-06 23:54:11 -0800

    Code refactoring - app/get/repositories (#161)

--- src/app/get/repositories/page.tsx
+++ src/app/get/repositories/page.tsx
@@ -1,13 +1,14 @@
-'use client'
 import React, { Suspense } from 'react'
 import Loading from '@/app/get/repositories/loading'
-import ListAvailableWiki from '@/app/get/repositories/ListAvailableWiki'
+import RepositoryList from '@/app/get/repositories/RepositoryList'
 
 export default function Page() {
     return (
         <div>
             <h1 className="text-3xl font-bold p-4 text-center" >Available Wiki Pages</h1>
-                <ListAvailableWiki />
+            <Suspense fallback={<Loading />}>
+                <RepositoryList />
+            </Suspense>
         </div>
     )
 }
